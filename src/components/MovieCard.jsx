2661ed2992d481651723eae41d34197ce554e6de
Malzagic <przemek2142@gmail.com>
2024-03-13 17:07:45 +0100

    filter movie search in moviecard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -16,27 +16,32 @@ function MovieCard() {
     );
   };
 
-  console.log(data)
+  console.log(data);
 
   if (!data || data.length < 0) return;
 
-  return data.map((item) => (
-    <div className="row" data-id={item.id} key={item.id} onClick={getID}>
-      <div className="column">
-        <div className="card">
-          <img
-            src={`https://image.tmdb.org/t/p/w500${item.backdrop_path}`}
-            alt={`${item.title}`}
-            className="thumbnail"
-          />
-          <h3>{item.title}</h3>
-          <Link className="reviews" to="/movie-reviews">
-            Reviews
-          </Link>
+  return data.map(
+    (item) =>
+      item.backdrop_path && (
+        <div className="row" data-id={item.id} key={item.id} onClick={getID}>
+          <div className="column">
+            <div className="card">
+              <img
+                src={`https://image.tmdb.org/t/p/w500${
+                  item.backdrop_path && item.backdrop_path
+                }`}
+                alt={`${item.title}`}
+                className="thumbnail"
+              />
+              <h3>{item.title}</h3>
+              <Link className="reviews" to="/movie-reviews">
+                Reviews
+              </Link>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  ));
+      )
+  );
 }
 
 export default MovieCard;
